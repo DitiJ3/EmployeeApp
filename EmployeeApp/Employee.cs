55ff8ed0c49e9a087d7e601abc529c39d6663555
eruditj <eruditj@RITK-NBO047>
2024-09-26 17:22:57 +0200

    Add project files.

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(string firstName, string lastName, int age, string position, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Position = position;
+            Salary = salary;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee: {FirstName} {LastName}, Age: {Age}, Position: {Position}, Salary: {Salary:C}");
+        }
+
+        public void GiveRaise(decimal percentage)
+        {
+            Salary += Salary * (percentage / 100);
+            Console.WriteLine($"{FirstName} {LastName} got a raise of {percentage}%. New Salary: {Salary:C}");
+        }
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Erudit", "Jupolli", 20, "DevOps Engineer", 100000m);
+            Employee employee2 = new Employee("Dea", "Simnica", 20, "Frontend Developer", 120000m);
+
+            employee1.DisplayEmployeeInfo();
+            employee2.DisplayEmployeeInfo();
+
+            employee2.GiveRaise(10);
+
+            Console.ReadLine(); 
+        }
+    }
+}
